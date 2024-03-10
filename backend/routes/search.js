24859af8479833338a3f3e71cbb32b539a3fee5b
itsakAdarsh <adarsh098765@gmail.com>
2024-03-10 00:04:44 -0500

    added autocomplete controller

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 const router = express.Router();
 router.get('/search', async (req, res) => {
     const client = new MongoClient(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -18,8 +18,9 @@ router.get('/search', async (req, res) => {
                     index: 'searchUsers',
                     text: {
                         query: searchQuery,
-                        path: {
-                            wildcard: '*'
+                        path: ["firstName", "lastName", "username", "emailAddress"],
+                        fuzzy: {
+                            maxEdits: 2
                         }
                     }
                 }
@@ -34,5 +35,39 @@ router.get('/search', async (req, res) => {
         await client.close();
     }
 });
+router.get('/try', async (req, res) => {
+    const client = new MongoClient(process.env.DATABASE_URL, { useNewUrlParser: true });
 
+    try {
+        await client.connect();
+
+        const database = client.db('friendshub_database');
+        const usersCollection = database.collection('useraccounts');
+
+        const searchQuery = req.query.q;
+
+        const searchResults = await usersCollection.aggregate([
+            {
+                $search: {
+                    index: 'searchUsers',
+                    autocomplete: {
+                        query: searchQuery,
+                        path: "firstName",
+                        tokenOrder: "any",
+                        fuzzy: {
+                            maxEdits: 2
+                        }
+                    }
+                }
+            }
+        ]).toArray();
+
+        res.json(searchResults);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
 export default router;
